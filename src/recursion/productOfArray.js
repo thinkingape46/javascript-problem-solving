f0f922b66e0073b835878af87638f3a1727b7c8d
jagadeesh <jagadeesh.kumar2091@gmail.com>
2025-02-25 16:13:15 +0000

    new problem solved

---
+++ src/recursion/productOfArray.js
@@ -0,0 +1,17 @@
+/*
+
+productOfArray
+Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+
+*/
+
+function productOfArray(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  return arr[0] + productOfArray(arr.slice(1));
+}
+
+console.log(productOfArray([1, 2, 3])); // 6
+console.log(productOfArray([1, 2, 3, 10])); // 60
