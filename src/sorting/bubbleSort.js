47af61735b09ff65dfd2ed01849ce3fda5caf961
jagadeesh <jagadeesh.kumar2091@gmail.com>
2025-04-15 21:00:39 +0530

    updated bubble sort

--- src/sorting/bubbleSort.js
+++ src/sorting/bubbleSort.js
@@ -14,7 +14,6 @@ function bubbleSort(arr) {
       count++;
 
       if (arr[i] > arr[i + 1]) {
-        console.log(i, arr[i + 1]);
         arr[i] = arr[i] + arr[i + 1];
         arr[i + 1] = arr[i] - arr[i + 1];
         arr[i] = arr[i] - arr[i + 1];
@@ -26,11 +25,11 @@ function bubbleSort(arr) {
     }
   }
 
-  return [arr, count];
+  return arr;
 }
 
-// console.log(bubbleSort([2, 3, 7, 4, 2, 8, 4, 3]));
+console.log(bubbleSort([2, 3, 7, 4, 2, 8, 4, 3]));
 console.log(bubbleSort([42, 7, 85, 19, 63, 28, 94, 3, 56, 71, 1]));
-// console.log(bubbleSort([64, 34, 25, 12, 22, 11, 90]));
-// console.log(bubbleSort([11, 12, 22, 25, 34, 64, 90]));
-// console.log(bubbleSort([1, 2, 3, 4, 5, 6, 7]));
+console.log(bubbleSort([64, 34, 25, 12, 22, 11, 90]));
+console.log(bubbleSort([11, 12, 22, 25, 34, 64, 90]));
+console.log(bubbleSort([1, 2, 3, 4, 5, 6, 7]));
