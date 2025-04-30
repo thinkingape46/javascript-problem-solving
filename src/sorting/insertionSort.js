995bb301eae132271e69b74d4deca935557b1764
jagadeesh <jagadeesh.kumar2091@gmail.com>
2025-04-30 18:10:50 +0530

    cleanup insertion sort

--- src/sorting/insertionSort.js
+++ src/sorting/insertionSort.js
@@ -12,7 +12,10 @@ function insertionSort(arr, comparator) {
 
     for (let j = 0; j < i; j++) {
       count++;
-      if (ele < arr[j] || (comparator && comparator(arr[j], ele) > 1)) {
+
+      const innerEle = arr[j];
+
+      if (ele < innerEle || (comparator && comparator(innerEle, ele) > 1)) {
         arr.splice(i, 1);
         arr.splice(j, 0, ele);
         break;
@@ -20,33 +23,20 @@ function insertionSort(arr, comparator) {
     }
   }
 
-  //   console.log(JSON.stringify(arr), arrLen, count);
   return arr;
 }
 
+// Testing
+
+console.log(insertionSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
+console.log(insertionSort([0, -10, 7, 4]));
+console.log(insertionSort([1, 2, 3]));
+console.log(insertionSort([]));
 console.log(
-  JSON.stringify(insertionSort([4, 20, 12, 10, 7, 9])) ===
-    JSON.stringify([4, 7, 9, 10, 12, 20])
-); // [4, 7, 9, 10, 12, 20]
-console.log(
-  JSON.stringify(insertionSort([0, -10, 7, 4])) ===
-    JSON.stringify([-10, 0, 4, 7])
-);
-console.log(
-  JSON.stringify(insertionSort([1, 2, 3])) === JSON.stringify([1, 2, 3])
-);
-console.log(JSON.stringify(insertionSort([])) === JSON.stringify([]));
-console.log(
-  JSON.stringify(
-    insertionSort([
-      4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67,
-      4342, 32,
-    ])
-  ) ===
-    JSON.stringify([
-      2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67, 75, 232, 232, 453,
-      546, 4342,
-    ])
+  insertionSort([
+    4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67,
+    4342, 32,
+  ])
 );
 
 var kitties = ["LilBub", "Garfield", "Heathcliff", "Blue", "Grumpy"];
@@ -60,11 +50,7 @@ function strComp(a, b) {
   return 0;
 }
 
-console.log(
-  JSON.stringify(insertionSort(kitties, strComp)) ===
-    JSON.stringify(["Blue", "Garfield", "Grumpy", "Heathcliff", "LilBub"])
-);
-// insertionSort(kitties, strComp); // ["Blue", "Garfield", "Grumpy", "Heathcliff", "LilBub"]
+console.log(JSON.stringify(insertionSort(kitties, strComp)));
 
 var moarKittyData = [
   {
@@ -93,4 +79,4 @@ function oldestToYoungest(a, b) {
   return b.age - a.age;
 }
 
-console.log(JSON.stringify(insertionSort(moarKittyData, oldestToYoungest)));
+console.log(insertionSort(moarKittyData, oldestToYoungest));
