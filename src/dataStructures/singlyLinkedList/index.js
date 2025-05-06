a29148048d2cd3000eed65257ee83b6f8f3f414e
jagadeesh <jagadeesh.kumar2091@gmail.com>
2025-05-06 23:32:34 +0530

    started working on single linked list

---
+++ src/dataStructures/singlyLinkedList/index.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  #head;
+  #tail;
+  constructor() {
+    this.#head = null;
+    this.#tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const node = new Node(value);
+
+    if (!this.#head) {
+      this.#head = node;
+    } else {
+      let currentValue = this.#head;
+      for (let i = 1; i <= this.length; i++) {
+        if (i === this.length) {
+          currentValue.next = node;
+        } else {
+          currentValue = currentValue.next;
+        }
+      }
+    }
+
+    this.#tail = node;
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    let value = this.#head;
+    for (let i = 1; i < this.length - 1; i++) {
+      value = value.next;
+
+      // Penultimate position -> new tail
+      if (i === this.length - 2) {
+        this.#tail = value;
+        this.#tail.next = null;
+      }
+    }
+    this.length--;
+  }
+
+  get tail() {
+    return this.#tail;
+  }
+}
+
+const a = new SinglyLinkedList();
+
+a.push(1);
+a.push(2);
+a.push(3);
+a.push(4);
+a.push(5);
+a.pop();
+a.pop();
+console.log(a);
