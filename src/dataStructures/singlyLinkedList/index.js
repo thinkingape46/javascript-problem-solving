2e073624de2b6ddc76c4ff0de061e7e4ea0af867
jagadeesh <jagadeesh.kumar2091@gmail.com>
2025-05-10 01:52:29 +0530

    updated pop and add shift

--- src/dataStructures/singlyLinkedList/index.js
+++ src/dataStructures/singlyLinkedList/index.js
@@ -27,18 +27,44 @@ class SinglyLinkedList {
   }
 
   pop() {
-    let value = this.head;
-    for (let i = 1; i < this.length - 1; i++) {
-      value = value.next;
+    let current = this.head;
+    let newTail = current;
 
-      // Penultimate position -> new tail
-      if (i === this.length - 2) {
-        this.tail = value;
-        this.tail.next = null;
-      }
+    if (!this.length) {
+      return undefined;
     }
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return current;
+    }
+
+    while (current?.next) {
+      newTail = current;
+      current = current?.next;
+    }
+
+    this.tail = newTail;
+    newTail.next = null;
     this.length--;
-    return this;
+    return current;
+  }
+
+  shift() {
+    const currentHead = this.head;
+    if (!this.length) {
+      return undefined;
+    }
+
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+
+    return currentHead;
   }
 }
 
@@ -46,7 +72,12 @@ const list = new SinglyLinkedList();
 
 list.push(1);
 list.push(2);
-list.push(3);
-list.push(4);
-list.push(5);
-console.log(list.head);
+// list.push(3);
+// list.push(4);
+// list.push(5);
+
+console.log(list.shift());
+console.log(list.shift());
+// console.log(list.shift());
+
+console.log(list);
